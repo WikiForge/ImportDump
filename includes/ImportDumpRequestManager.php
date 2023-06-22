bc3e90afd02e28f7dc77d1e79826e195caf3aa85
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-22 13:40:41 -0600

    Add {file-name} option to command

--- includes/ImportDumpRequestManager.php
+++ includes/ImportDumpRequestManager.php
@@ -398,11 +398,13 @@ class ImportDumpRequestManager {
 			'{IP}',
 			'{wiki}',
 			'{username-prefix}',
-			'{file}',
+			'{file-name}',
+			'{file-path}',
 		], [
 			MW_INSTALL_PATH,
 			$this->getTarget(),
 			$this->getInterwikiPrefix(),
+			$this->getTarget() . '-' . $this->getTimestamp() . '.xml',
 			FileBackend::splitStoragePath( $this->getFilePath() )[2],
 		], $command );
 	}
